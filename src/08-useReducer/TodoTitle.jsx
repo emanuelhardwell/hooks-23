042edddd89c99596dc04780abe922e1b558c23c3
emanuelhardwell <emanuelnoe16@gmail.com>
2023-03-12 14:36:22 -0600

    feat: The TodoTitle was created

---
+++ src/08-useReducer/TodoTitle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const TodoTitle = () => {
+  return (
+    <>
+      <div className="col-md-12 mt-5">
+        <h1 className="text-center text-primary">TodoApp</h1>
+      </div>
+    </>
+  );
+};
