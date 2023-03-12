954ffa728595dad6163fb202aaadf1462ccb9890
emanuelhardwell <emanuelnoe16@gmail.com>
2023-03-12 14:22:30 -0600

    feat: The components TodoList and TodoListItem was created

---
+++ src/08-useReducer/TodoList.jsx
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+import { TodoListItem } from "./TodoListItem";
+
+export const TodoList = ({ todos }) => {
+  return (
+    <>
+      <ul className="list-group mx-5">
+        {todos.map((todo) => (
+          <TodoListItem key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+TodoList.propTypes = {
+  todos: PropTypes.array.isRequired,
+};
